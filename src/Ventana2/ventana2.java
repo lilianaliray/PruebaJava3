123bb679ba0dd4f17189bfc5cefa1a80e39a4e1d
lilianaliray <marcelo_masoka@hotmail.com>
2017-11-10 19:32:50 -0300

    Paso 9- llamado a la clase JFrame

--- src/Ventana2/ventana2.java
+++ src/Ventana2/ventana2.java
@@ -8,10 +8,32 @@ package Ventana2;
 import Ventana2.ventana2;// import paquete.clases
 import javax.swing.JFrame;
 
-/**
+/**************************************************************************
+ * nombre:liliana hevia valenzuela
+ * fecha:10-11-17
+ * clase:pruebajava3
+ * responsabilidad:
  *
  * @author LAB04
  */
 public class ventana2 {
+    public static void main(String[] args)
+    {
+         // TODO code application logic here
+        //Paso 9- llamado a la clase JFrame
+      
+        ventana2 ventana2 = new ventana2();// crea un objeto tipo ventana2
+        ventana2.setVisible(true);
+        ventana2.setBounds(0, 0,600, 800);
+        // la siguiente inst. le pone titulo a la ventana
+        ventana2.setTitle("Mi primer interfaz grafica");
+        // la siguiente sentencia es para finalizar el proyecto al cerrar
+        ventana2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
     
 }
+
+
+    
+
