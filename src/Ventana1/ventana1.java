8fed7be03d65d30f3b627820e04000064b94b478
lilianaliray <marcelo_masoka@hotmail.com>
2017-11-10 19:11:30 -0300

    paso 2 -heredar

--- src/Ventana1/ventana1.java
+++ src/Ventana1/ventana1.java
@@ -6,11 +6,15 @@
 package Ventana1;
 import javax.swing.*;
 import java.awt.*;
-/**
- *
+/*****************************************************************
+ *nombre:liliana hevia valenzuela
+ * fecha:10-11-17
+ * clase:pruebajava3
+ * responsabilidad:
  * @author LAB04
  */
-public class ventana1 
+// paso 2 aca se heredar
+public class  ventana1 extends JFrame 
 {
  
 }
